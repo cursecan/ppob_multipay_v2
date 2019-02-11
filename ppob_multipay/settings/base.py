269dfb93f3e3d1858782166b59ea7badf8bf677b
Anderi Setiawan <anderi.setiawan@gmail.com>
2019-02-11 09:08:55 +0700

    Mod static configuraton

--- ppob_multipay/settings/base.py
+++ ppob_multipay/settings/base.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'env_root', 'static_root')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
