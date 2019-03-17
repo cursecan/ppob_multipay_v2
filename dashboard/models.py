5b0910e5b19c365d9dd39782d7698025e22f7e94
Anderi Setiawan <anderi.setiawan@gmail.com>
2019-03-17 18:50:38 +0700

    Release v 1.6 user type view

--- dashboard/models.py
+++ dashboard/models.py
@@ -4,7 +4,8 @@ from core.models import CommonBase
 
 class Application(CommonBase):
     app_name = models.CharField(max_length=100)
-    attach = models.FileField(upload_to='application/')
+    attach = models.FileField(upload_to='application/', blank=True, null=True)
+    link = models.URLField(max_length=200, blank=True)
 
     def __str__(self):
         return self.app_name
